67c2f377a1563eac33dab556cac5034d1f7caa8c
alexcat <alexkatishev@yandex.ru>
2021-12-01 10:58:09 +0300

    init commit

--- app/src/main/java/ru/alexcat/goldenring/MainActivity.kt
+++ app/src/main/java/ru/alexcat/goldenring/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        
+
+
+
     }
 }
