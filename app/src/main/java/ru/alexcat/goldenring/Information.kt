9b6194d016a946598fac124b542f00cc0fe5fee6
alexcat <alexkatishev@yandex.ru>
2021-12-09 11:19:49 +0300

    init commit

--- app/src/main/java/ru/alexcat/goldenring/Information.kt
+++ app/src/main/java/ru/alexcat/goldenring/Information.kt
@@ -1,10 +1,27 @@
 package ru.alexcat.goldenring
+
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class Information : AppCompatActivity() {
+    private val url1 : String ="https://tourweek.ru/articles/world/277593"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val goUri = findViewById<View>(R.id.btn_uri)
+
+        goUri.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url1)
+                      startActivity(intent)
+        }
     }
 }
